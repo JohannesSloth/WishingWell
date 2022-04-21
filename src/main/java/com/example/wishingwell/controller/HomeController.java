faaf427139fec58f98e776e7848a0815ce3c3b75
Shortytheman <shor7ty@gmail.com>
2022-04-21 11:29:55 +0200

    yo1

--- src/main/java/com/example/wishingwell/controller/HomeController.java
+++ src/main/java/com/example/wishingwell/controller/HomeController.java
@@ -1,14 +1,37 @@
 package com.example.wishingwell.controller;
 
+import com.example.wishingwell.repository.WishRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
+  WishRepository repository;
+
   @GetMapping("/")
   public String index(){
     return "index";
+  }
+
+
+  @GetMapping("/addwish")
+  public String addwish(HttpSession httpSession){
+    httpSession.setAttribute("wishlist",repository.getall());
+    return "/addwish";
+  }
+
+  @PostMapping("/addwish")
+    public String addWish(@RequestParam String name, @RequestParam double price, @RequestParam String url, @RequestParam
+                          String pictureurl, @RequestParam String description){
+      repository.addWish(new Wish (name,price,url,pictureurl,description));
+      return "redirect:/";
+    }
   }
 
 }
--- src/main/java/com/example/wishingwell/repository/WishRepository.java
+++ src/main/java/com/example/wishingwell/repository/WishRepository.java
@@ -1,9 +1,17 @@
 package com.example.wishingwell.repository;
 
+import com.example.wishingwell.model.Wish;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public class WishRepository {
 
+  public ArrayList<Wish> getall(){
+    ArrayList<Wish> wishes = new ArrayList<>();
+
+    return wishes;
+  }
 
 }
